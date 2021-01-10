19c23d0615ad9a83d634802e231e5db55d0c5caa
rgroves <rwgdev@gmail.com>
2021-01-10 14:18:20 -0600

    Pass fifth user story.

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -4,6 +4,9 @@ export default function QuoteBox() {
       <div id="text">Quote goes here!</div>
       <div id="author">— Quote Author</div>
       <button id="new-quote">New Quote</button>
+      <a id="tweet-quote" heref="#">
+        Tweet Quote
+      </a>
     </div>
   );
 }
